930a8faa80d52c0bcfc02c777ad37b3932f66d1b
vahidGhadiri <whydrf@gmail.com>
2021-12-02 18:37:55 +0330

    Sidebar implement

--- src/components/Sidebar/Sidebar.style.tsx
+++ src/components/Sidebar/Sidebar.style.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import {colors as COLORS} from "../../assets/colors";
 
 export const Container = styled.div`
+  overflow: scroll;
   width: 25rem;
   height: 100vh;
   border-right: 1px solid ${COLORS.BORDER};
